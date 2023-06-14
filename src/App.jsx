4f51ca2badcc317c5bb0e2b9a8ecfaa00634fba9
Abián <abian.camejo@gmail.com>
2023-06-14 16:06:31 +0100

    card in process

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="card border-2 border-black text-red-700 w-96 h-96">
+        <div className="w-full">
+          
+        </div> 
+      </div>
+    </>
+  );
+}
+
+export default App;
