75442fc82d890e1d0511108ea598e808a3460bee
Abián <abian.camejo@gmail.com>
2023-06-16 12:04:35 +0100

    need to add the error style and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,104 @@
-import "./App.css"
-import icon from "./assets/images/icon-arrow.svg"
-import React, {useState, useEffect } from 'react';
-
-
+import "./App.css";
+import icon from "./assets/images/icon-arrow.svg";
+import { useState } from "react";
 
 function App() {
+  const [userDay, setUserDay] = useState([]);
+  const [userMonth, setUserMonth] = useState([]);
+  const [userYear, setUserYear] = useState([]);
+  const [daysCalc, setDayCalc] = useState("- -");
+  const [monthCalc, setMonthCalc] = useState("- -");
+  const [yearCalc, setYearCalc] = useState("- -");
 
-const [day, setDay] = useState([])
-const [month, setMonth] = useState([])
-const [year, setYear] = useState([])
+  function ageCalc() {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = Math.round(today.getMonth() + 1);
+    let actualDay = today.getDate();
+    let monthDays = new Date(userYear, userMonth, 0).getDate();
+    let substractMonth = [];
+    let substractDay = [];
 
- const ageCalc = () => {
-  let today = new Date()
-  console.log(today.getFullYear() - year)
-  console.log(Math.round(today.getMonth() +1 - month))
-  console.log(today.getDate() - day)
-}
-
-ageCalc()
-
-const handleChange = () => {
-  console.log(day, month, year)
-}
-
-useEffect(() => {
-  handleChange()
-}, [])
+    if (
+      year < userYear ||
+      userYear < 1907 ||
+      userMonth < 1 ||
+      userMonth > 12 ||
+      userDay > monthDays ||
+      userDay < 1
+    ) {
+        alert("error");
+    } else if (yearCalc && monthCalc && daysCalc) {
+        setYearCalc(year - userYear);
+        substractMonth.push(month, parseInt(userMonth));
+        substractMonth.sort(function (a, b) {
+        return b - a;
+      });
+        setMonthCalc(substractMonth[0] - substractMonth[1]);
+        substractDay.push(actualDay, parseInt(userDay));
+        substractDay.sort(function (a, b) {
+        return b - a;
+      });
+      setDayCalc(substractDay[0] - substractDay[1]);
+    }
+  }
 
   return (
     <>
       <div className="card border-2 space-x-10 bg-white">
-          <div className="inputText space-x-20 mt-8 flex flex-row ml-14">
-            <div><b>DAY</b></div>
-            <div><b>MONTH</b></div>
-            <div><b>YEAR</b></div>
+        <div className="inputText space-x-20 mt-8 flex flex-row ml-14">
+          <div>
+            <b>DAY</b>
           </div>
+          <div>
+            <b>MONTH</b>
+          </div>
+          <div>
+            <b>YEAR</b>
+          </div>
+        </div>
         <div className="inputContainer space-x-6 mt-2 flex flex-row h-10">
-          <input className="day w-24 flex flex-row border-2" placeholder=" DD" value={day} onChange={(e) => setDay(e.target.value)}></input>
-          <input className="month w-24 flex flex-row border-2" placeholder=" MM " value={month} onChange={(e) => setMonth(e.target.value)}></input>
-          <input className="year w-24 flex flex-row border-2" placeholder=" YYYY" value={year} onChange={(e) => setYear(e.target.value)}></input>
-          </div>
-          <div className="flex">
+          <input
+            className="day w-24 flex flex-row border-2"
+            placeholder=" DD"
+            value={userDay}
+            onChange={(e) => setUserDay(e.target.value)}
+          ></input>
+          <input
+            className="month w-24 flex flex-row border-2"
+            placeholder=" MM "
+            value={userMonth}
+            onChange={(e) => setUserMonth(e.target.value)}
+          ></input>
+          <input
+            className="year w-24 flex flex-row border-2"
+            placeholder=" YYYY"
+            value={userYear}
+            onChange={(e) => setUserYear(e.target.value)}
+          ></input>
+        </div>
+        <div className="flex">
           <div className="border mt-10 w-96 h-0 flex" />
-          <button className="bg-white border rounded-full border-none outline-none no-outline-button" onClick={() => handleChange()}>
-          <img src={icon} className="icon mt-1 border rounded-full bg-[#854dff] p-3"/>
+          <button
+            className="bg-white border rounded-full border-none outline-none no-outline-button"
+            onClick={() => ageCalc()}
+          >
+            <img
+              src={icon}
+              className="icon mt-1 border rounded-full bg-[#854dff] p-3"
+            />
           </button>
-          </div>
+        </div>
         <div className="ageCalc">
-          <div className="flex text-black "><div className="dateOutput">- -</div>years</div>
-          <div className="flex text-black"><div className="dateOutput">- -</div>months</div>
-          <div className="flex text-black"><div className="dateOutput">- -</div>days</div>
+          <div className="flex text-black ">
+            <div className="dateOutput">{yearCalc}</div>years
+          </div>
+          <div className="flex text-black">
+            <div className="dateOutput">{monthCalc}</div>months
+          </div>
+          <div className="flex text-black">
+            <div className="dateOutput">{daysCalc}</div>days
+          </div>
         </div>
       </div>
     </>
